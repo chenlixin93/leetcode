577d02461bfa9282f70a5ec9d30c3bc9ce247f1c
jasper <jasper.chen@sain3.com>
2020-06-28 08:50:34 +0800

    code 4th

---
+++ 42.接雨水.php
@@ -0,0 +1,20 @@
+<?php
+/*
+ * @lc app=leetcode.cn id=42 lang=php
+ *
+ * [42] 接雨水
+ */
+
+// @lc code=start
+class Solution {
+
+    /**
+     * @param Integer[] $height
+     * @return Integer
+     */
+    function trap($height) {
+        
+    }
+}
+// @lc code=end
+
