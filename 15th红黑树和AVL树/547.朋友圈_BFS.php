b7d813083082db1836b8c79d213a0b8c1d8b7b83
jasper <jasper.chen@sain3.com>
2020-08-07 17:03:58 +0800

    code 12+th

---
+++ 15th红黑树和AVL树/547.朋友圈_BFS.php
@@ -0,0 +1,51 @@
+<?php
+/*
+ * @lc app=leetcode.cn id=547 lang=php
+ *
+ * [547] 朋友圈
+ * 
+ * https://leetcode-cn.com/problems/friend-circles/
+ */
+
+// @lc code=start
+class Solution {
+
+    /**
+     * @param Integer[][] $M
+     * @return Integer
+     */
+    function findCircleNum($M) {
+
+        $n = count($M);
+        $res = 0;
+
+        $visited = array_fill(0, $n, 0);
+        $queue = [];
+
+        for ($i=0; $i < $n; $i++) { 
+            if ($visited[$i]) continue;
+
+            array_push($queue, $i);
+            while (!empty($queue)) {
+                $t = array_shift($queue);
+                $visited[$t] = 1;
+
+                for ($j=0; $j < $n; $j++) { 
+                    if ($M[$t][$j] == 0 || $visited[$j]) continue;
+
+                    array_push($queue, $j);
+                }
+            }
+
+            $res++;
+        }
+
+        return $res;
+    }
+}
+
+// Accepted
+// 113/113 cases passed (176 ms)
+// Your runtime beats 11.76 % of php submissions
+// Your memory usage beats 100 % of php submissions (17.1 MB)
+// @lc code=end
---
+++ 15th红黑树和AVL树/547.朋友圈_UnionFind.php
@@ -0,0 +1,57 @@
+<?php
+/*
+ * @lc app=leetcode.cn id=547 lang=php
+ *
+ * [547] 朋友圈
+ * 
+ * https://leetcode-cn.com/problems/friend-circles/
+ */
+
+// @lc code=start
+class Solution {
+
+    /**
+     * @param Integer[][] $M
+     * @return Integer
+     */
+    function findCircleNum($M) {
+
+        $n = count($M);
+        $res = $n;
+        $root = array_fill(0, $n, 0);
+        for ($i=0; $i < $n; $i++) { 
+            $root[$i] = $i;
+        }
+
+        for ($i=0; $i < $n; $i++) { 
+            for ($j=$i+1; $j < $n; $j++) { 
+                if ($M[$i][$j] == 1) {
+                    $p1 = $this->getRoot($root, $i);
+                    $p2 = $this->getRoot($root, $j);
+                    if ($p1 != $p2) {
+                        $res--;
+                        $root[$p2] = $p1;
+                    }
+                }
+            }
+        }
+
+        return $res;
+    }
+
+    function getRoot(&$root, $i) {
+        while ($i != $root[$i]) {
+            $root[$i] = $root[$root[$i]];
+            $i = $root[$i];
+        }
+
+        return $i;
+    }
+}
+
+// Accepted
+// 113/113 cases passed (80 ms)
+// Your runtime beats 64.71 % of php submissions
+// Your memory usage beats 100 % of php submissions (17.1 MB)
+// @lc code=end
+
